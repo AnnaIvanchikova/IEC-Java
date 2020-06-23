90c5941939e9d3f1d34303398c4861a57f4fda3a
Anna-Ivanchikova <Anna.Ivanchikova@ibm.com>
2020-06-23 13:13:46 +0300

    first init commit

---
+++ IEC_Java/src/main/java/com/example/iecjava/IecJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.iecjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IecJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IecJavaApplication.class, args);
+	}
+
+}
