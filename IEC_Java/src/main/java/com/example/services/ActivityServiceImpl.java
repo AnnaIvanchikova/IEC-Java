50a463704d16b25795ae3e5a31686f2998d4855f
Anna-Ivanchikova <Anna.Ivanchikova@ibm.com>
2020-06-23 13:59:29 +0300

    some changes in service

--- IEC_Java/src/main/java/com/example/services/ActivityServiceImpl.java
+++ IEC_Java/src/main/java/com/example/services/ActivityServiceImpl.java
@@ -2,9 +2,17 @@ package com.example.services;
 
 import java.util.List;
 
-import com.example.entity.Activity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.example.entity.Activity;
+import com.example.repository.ActivityRepository;
+
+@Service
 public class ActivityServiceImpl implements ActivityService{
+	
+	@Autowired
+	private ActivityRepository activityRepository;
 
 	@Override
 	public List<Activity> getAllActivity() {
